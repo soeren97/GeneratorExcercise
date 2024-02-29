f929557385532eb11403a6c1ac17b2d1d0d04cf5
Søren <soeren@langkilde.com>
2024-02-29 11:31:33 +0100

    Added profiler

--- main.py
+++ main.py
@@ -1,31 +1,46 @@
 """Script to run the main loop."""
 
+import cProfile
 from os import listdir, makedirs
 
-from calculations import find_age
+from src.calculations import find_age
 from src.cleaning import clean_phone_number
-from src.data import generate_fake_dataset, save_dataset
+from src.data import create_dataset
 from src.generators import apply_functions_to_large_file
 
 
-def main(num_rows: int) -> None:
+def main(num_rows: int, debug: bool = False) -> None:
     """Run main loop.
 
     Args:
         num_rows (int): Desired number of rows in the fake dataset.
+        debug (bool): Activates debug mode. (Profiler and generate new dataset.)
     """
     makedirs("Data", exist_ok=True)
-    if "fakeData.csv" not in listdir("Data") or True:
-        generator = generate_fake_dataset(num_rows)
+    if "fakeData.csv" not in listdir("Data") or debug:
+        if not debug:
+            create_dataset("fakeData", num_rows)
+        else:
+            cProfile.run(f"create_dataset('fakeData', {num_rows})", sort="cumulative")
 
-        save_dataset(generator, "fakeData", num_rows)
-
-    apply_functions_to_large_file(
-        "fakeData.csv",
-        [clean_phone_number, find_age],
-        "cleanedNumbers",
-        num_rows,
-    )
+    if not debug:
+        apply_functions_to_large_file(
+            "fakeData.csv",
+            [clean_phone_number, find_age],
+            "cleanedNumbers",
+            num_rows,
+        )
+    else:
+        cProfile.run(
+            f"""apply_functions_to_large_file(
+                'fakeData.csv',
+                [clean_phone_number, find_age],
+                'cleanedNumbers',
+                {num_rows},
+                )
+                """,
+            sort="cumulative",
+        )
 
 
 if __name__ == "__main__":
